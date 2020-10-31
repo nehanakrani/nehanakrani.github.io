2904b60ee600fdae904bb8cf76c4d16f26210a22
Neha Nakrani <nehanakrani004@gmail.com>
2020-10-31 11:33:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,9 @@ var serveStatic = require('serve-static')
 
 var app = express()
 // app.use(serveStatic(path.join(__dirname, 'dist')))
-app.use(express.static(path.join(__dirname,"client", "build")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "build","client", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 var port = process.env.PORT || 8000
