2d2037e685420cbe35e5ba2bbe523b98e57a06ed
Neha Nakrani <nehanakrani004@gmail.com>
2020-10-31 11:21:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express')
 var path = require('path')
 var serveStatic = require('serve-static')
 
-// var app = express()
+var app = express()
 // app.use(serveStatic(path.join(__dirname, 'dist')))
 
 app.get("*", (req, res) => {
