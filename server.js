c5277c0960e60403fe9a093b98a3e47199e6c893
Neha Nakrani <nehanakrani004@gmail.com>
2020-10-31 11:18:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ var express = require('express')
 var path = require('path')
 var serveStatic = require('serve-static')
 
-var app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
+// var app = express()
+// app.use(serveStatic(path.join(__dirname, 'dist')))
+
+app.get("*", (req, res) => {
+  let url = path.join(__dirname, '../client/build', 'index.html');
+  if (!url.startsWith('/app/')) // since we're on local windows
+    url = url.substring(1);
+  res.sendFile(url);
+});
 
 var port = process.env.PORT || 8000
 app.listen(port)
